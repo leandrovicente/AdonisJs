8b7ce0596e3284f7a0e4881668cef894f3021432
leandro <leandro.vicente98@outlook.com>
2020-02-02 22:40:40 -0300

    Finalizado o modulo de recuperação de senha

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,7 +1,9 @@
 "use strict";
 
+const moment = require("moment");
 const crypto = require("crypto");
 const User = use("App/Models/User");
+const Mail = use("Mail");
 class ForgotPasswordController {
   async store({ request, response }) {
     try {
@@ -11,10 +13,51 @@ class ForgotPasswordController {
       user.token = crypto.randomBytes(10).toString("hex");
       user.token_created_at = new Date();
       await user.save();
+
+      await Mail.send(
+        ["emails.forgot_password"],
+        {
+          email,
+          token: user.token,
+          link: `${request.input("redirect_url")}?token=${user.token}`
+        },
+        message => [
+          message
+            .to(user.email)
+            .from("leandro.vicente98@outlook.com", "Leandro Vicente")
+            .subject("Recuperação de Senha")
+        ]
+      );
     } catch (err) {
       return response.status(err.status).send({
-        error: { message: "Algo não deu certo,  esse e-mail existe?" }
+        error: { message: "Algo não deu certo, esse e-mail existe?" }
       });
+    }
+  }
+
+  async update({ request, response }) {
+    try {
+      const { token, password } = request.all();
+
+      const user = await User.findByOrFail("token", token);
+
+      const tokenExpired = moment()
+        .subtract("2", "days")
+        .isAfter(user.token_created_at);
+
+      if (tokenExpired) {
+        return response
+          .status(401)
+          .send({ error: { message: "O Token de recuperação está expirado" } });
+      }
+      user.token = null;
+      user.token_created_at = null;
+      user.password = password;
+      await user.save();
+    } catch (err) {
+      return response
+        .status(err.status)
+        .send({ error: { message: "Algo deu errado ao resetar sua senha" } });
     }
   }
 }
